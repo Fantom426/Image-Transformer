a7f76c85decee679904070b4c4e842a1a6163f29
Вадим <Vadik426_426@mail.ru>
2017-09-26 22:13:21 +0300

    Add files via upload

---
+++ Image Transformer/Lab1/Lab1/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace Lab1
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(25, 211);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Открыть";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(168, 26);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(436, 382);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Тип преобразования";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(23, 182);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(105, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Преобразовать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Порог";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(64, 109);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(63, 20);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "10";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(23, 42);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(92, 17);
+            this.radioButton1.TabIndex = 7;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Бинаризация";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(23, 65);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(104, 17);
+            this.radioButton2.TabIndex = 8;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Биполяризация";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(27, 254);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox2.TabIndex = 10;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(610, 26);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(447, 382);
+            this.pictureBox3.TabIndex = 11;
+            this.pictureBox3.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(61, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Интервал";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(33, 156);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(49, 20);
+            this.textBox2.TabIndex = 14;
+            this.textBox2.Text = "0";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(113, 156);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(49, 20);
+            this.textBox3.TabIndex = 15;
+            this.textBox3.Text = "10";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 159);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(18, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "от";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(88, 159);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(19, 13);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "до";
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(23, 89);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(119, 17);
+            this.radioButton3.TabIndex = 18;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Масштабирование";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1069, 447);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.RadioButton radioButton1;
+		private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RadioButton radioButton3;
+    }
+}
+
---
+++ Image Transformer/Lab1/Lab1/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Lab1
+{
+	public partial class Form1 : Form
+	{
+		Bitmap image;
+        Bitmap image2;
+
+        int[,] Res;
+        double[,] Res2;
+        string path1 = @"C:\NN\binarization.txt";
+        string path2 = @"C:\NN\bipolarization.txt";
+        string path3 = @"C:\NN\mashtab.txt";
+
+
+        public Form1()
+		{
+			InitializeComponent();
+		}
+
+
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+            
+			OpenFileDialog open_dialog = new OpenFileDialog(); 
+			open_dialog.Filter = "Image Files(*.BMP;*.JPG;*.GIF;*.PNG)|*.BMP;*.JPG;*.GIF;*.PNG|All files (*.*)|*.*";
+            string filename = open_dialog.FileName;
+			if (open_dialog.ShowDialog() == DialogResult.OK) 
+			{
+				try
+				{
+                    image = new Bitmap(open_dialog.FileName); 
+                    this.pictureBox1.Size = image.Size; 
+                    pictureBox1.Image = image;
+					pictureBox1.Invalidate();
+				}
+				catch
+				{
+					DialogResult result = MessageBox.Show("Невозможно открыть выбранный файл",
+					"Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				}
+			}
+		}
+
+		private void pictureBox1_Click(object sender, EventArgs e)
+		{
+			
+		}
+
+		private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+		{
+			
+		}
+
+		private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+		{
+            // if (e.Button == MouseButtons.Left)
+            
+                Color pixelColor = image.GetPixel(e.X, e.Y);
+                pictureBox2.BackColor = pixelColor;
+                SolidBrush pixelBrush = new SolidBrush(pixelColor);
+            
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Res = new int[image.Width, image.Height];
+            Res2 = new double[image.Width, image.Height];
+            StreamWriter sw;
+            if (radioButton1.Checked)
+            {
+                int average = (int)(pictureBox2.BackColor.R + pictureBox2.BackColor.G + pictureBox2.BackColor.B) / 3;
+                image2 = new Bitmap(image);
+                sw = new StreamWriter(path1);
+                for (int i = 0; i < image.Width; i++)
+                {
+                    for (int j = 0; j < image.Height; j++)
+                    {
+                        Color pixel = image.GetPixel(i, j);
+                        int average2 = (int)(pixel.R + pixel.G + pixel.B) / 3;
+                        if (average2 >= average - Convert.ToInt32(textBox1.Text) && average2 <= average + Convert.ToInt32(textBox1.Text))
+                        {
+                            image2.SetPixel(i, j, Color.Black);
+                            Res[i, j] = 1;
+                            sw.Write(Convert.ToString(Res[i, j]));
+                        }
+                        else
+                        {
+                            image2.SetPixel(i, j, Color.White);
+                            Res[i, j] = 0;
+                            sw.Write(Convert.ToString(Res[i, j]));
+                        }
+
+                    }
+                    sw.WriteLine();
+                }
+                pictureBox3.Image = image2;
+                sw.Close();
+            }
+            
+            if (radioButton2.Checked)
+            {
+                int average = (int)(pictureBox2.BackColor.R + pictureBox2.BackColor.G + pictureBox2.BackColor.B) / 3;
+                image2 = new Bitmap(image);
+                sw = new StreamWriter(path2);
+                for (int i = 0; i < image.Width; i++)
+                {
+                    for (int j = 0; j < image.Height; j++)
+                    {
+                        Color pixel = image.GetPixel(i, j);
+                        int average2 = (int)(pixel.R + pixel.G + pixel.B) / 3;
+                        if (average2 >= average - Convert.ToInt32(textBox1.Text) && average2 <= average + Convert.ToInt32(textBox1.Text))
+                        {
+                            image2.SetPixel(i, j, Color.Black);
+                            Res[i, j] = 1;
+                            sw.Write(Convert.ToString(Res[i, j]));
+                        }
+                        else
+                        {
+                            image2.SetPixel(i, j, Color.White);
+                            Res[i, j] = -1;
+                            sw.Write(Convert.ToString(Res[i, j]));
+                        }
+                    }
+                    sw.WriteLine();
+                }
+                pictureBox3.Image = image2;
+                sw.Close();
+            }
+
+            if (radioButton3.Checked)
+            {
+                int average = (int)(pictureBox2.BackColor.R + pictureBox2.BackColor.G + pictureBox2.BackColor.B) / 3;
+                image2 = new Bitmap(image);
+                sw = new StreamWriter(path3);
+                int min = 0;
+                int max = 0;
+                for (int i = 0; i < image.Width; i++)
+                    for (int j = 0; j < image.Height; j++)
+                    {
+                        Color pixel = image.GetPixel(i, j);
+                        int average2 = (int)(pixel.R + pixel.G + pixel.B) / 3;
+                        if (average2 < min)
+                            min = average2;
+                        if (average2 > max)
+                            max = average2;
+                        if (average2 >= average - Convert.ToInt32(textBox1.Text) && average2 <= average + Convert.ToInt32(textBox1.Text))
+                        {
+                            image2.SetPixel(i, j, Color.Black);
+                            
+                        }
+                        else
+                        {
+                            image2.SetPixel(i, j, Color.White);
+                            
+                        }
+                    }
+                pictureBox3.Image = image2;
+                double a = Convert.ToDouble(textBox2.Text);
+                double b = Convert.ToDouble(textBox3.Text);
+
+                for (int i = 0; i < image.Width; i++)
+                {
+                    for (int j = 0; j < image.Height; j++)
+                    {
+                        Color pixel = image.GetPixel(i, j);
+                        double average2 = (double)(pixel.R + pixel.G + pixel.B) / 3;
+                        if (average2 == min)
+                            Res2[i, j] = a;
+                        else if (average2 == max)
+                            Res2[i, j] = b;
+                        else
+                            Res2[i, j] = (average2 / 255) * (b - a) + a;
+                        sw.Write(Convert.ToString(Res2[i, j]).Replace(',','.'));
+                        sw.Write(" ");
+                    }
+                    sw.WriteLine();
+                }
+                sw.Close();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
